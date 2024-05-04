fc738ed988a7dd0b785581aadf36799f19e14416
yamaan93 <yamaan93@gmail.com>
2024-05-04 17:06:46 -0400

    folder convert asks user for seperate output directory just in case

--- pivot_raw_data_folder.py
+++ pivot_raw_data_folder.py
@@ -7,11 +7,12 @@ import pandas as pd
 root = tk.Tk()
 root.withdraw()
 directory = filedialog.askdirectory()
+output_directory = filedialog.askdirectory()
 
 for filename in os.listdir(directory):
     file,extension = os.path.splitext(filename)
     if extension == ".csv":
         df = pd.read_csv(directory+"/"+filename)
         pivot_df = df.pivot_table(index='time_stamp', columns='signal_name', values='value', aggfunc='first')
-        pivot_df.to_csv(directory+"/"+file+"_pivoted.csv")
+        pivot_df.to_csv(output_directory+"/"+file+"_pivoted.csv")
         
