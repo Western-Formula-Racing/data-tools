6b237908ed602afc382204c3d9b6b4fb1bebfc3e
yamaan93 <yamaan93@gmail.com>
2024-05-25 09:14:09 -0400

    update everything

--- find_torque.py
+++ find_torque.py
@@ -7,11 +7,10 @@ import pandas as pd
 root = tk.Tk()
 root.withdraw()
 directory = filedialog.askdirectory()
-output_directory = filedialog.askdirectory()
 
 for filename in os.listdir(directory):
     file,extension = os.path.splitext(filename)
     if extension == ".csv":
         df = pd.read_csv(directory+"/"+filename)
-        if (df['torqueRequest']>200).any():
+        if 'inv_torque_request' in df.columns and (df['inv_torque_request']>150).any():
             print(directory+"/"+filename +": "+ (str)( os.path.getsize(directory+"/"+filename)))
